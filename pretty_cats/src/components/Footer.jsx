dec8673daec7a4f5e7b9e12285c61e80e5ec57da
louispaulet <louispaulet13@gmail.com>
2024-07-21 21:21:41 +0200

    add logo related files and logo to the navbar, update navbar with website title, make it clickable to redirect to homepage, update footer with website name

--- pretty_cats/src/components/Footer.jsx
+++ pretty_cats/src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
   return (
     <footer className="bg-gray-800 p-4 mt-auto w-full">
       <div className="container mx-auto text-center text-white">
-        <p>&copy; 2024 Your Company</p>
+        <p>&copy; 2024 Pretty Cats Inc.</p>
       </div>
     </footer>
   );
--- pretty_cats/src/components/Navbar.jsx
+++ pretty_cats/src/components/Navbar.jsx
@@ -1,10 +1,15 @@
-// src/components/Navbar.jsx
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <Link to="/" className="flex items-center">
+            <img src="/pretty_cat_logo.webp" alt="Pretty Cats Logo" className="h-8 w-8 mr-2" />
+            <span className="text-white text-xl font-bold">Pretty Cats</span>
+          </Link>
+        </div>
         <div className="text-white">
           <Link className="mr-4 hover:text-gray-400" to="/">Home</Link>
           <Link className="mr-4 hover:text-gray-400" to="/about">About</Link>
